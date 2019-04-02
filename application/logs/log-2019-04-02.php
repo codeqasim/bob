3c999d895d410f0ce82d913082fec2b14f73b107
qaxim <compoxition@gmail.com>
2019-04-02 20:26:09 +0500

    Update log-2019-04-02.php

--- application/logs/log-2019-04-02.php
+++ application/logs/log-2019-04-02.php
@@ -8,3 +8,11 @@ ERROR - 2019-04-02 17:16:38 --> Severity: Warning --> file_get_contents(http://l
  E:\server\htdocs\bob\application\controllers\Home.php 14
 ERROR - 2019-04-02 17:16:38 --> Severity: Notice --> Trying to get property 'data' of non-object E:\server\htdocs\bob\application\controllers\Home.php 14
 ERROR - 2019-04-02 17:16:38 --> Severity: error --> Exception: Call to undefined method Home::getOTAdata() E:\server\htdocs\bob\application\controllers\Home.php 15
+DEBUG - 2019-04-02 17:19:06 --> UTF-8 Support Enabled
+DEBUG - 2019-04-02 17:19:06 --> No URI present. Default controller set.
+DEBUG - 2019-04-02 17:19:06 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2019-04-02 17:19:06 --> Config file loaded: E:\server\htdocs\bob\application\config/theme.php
+ERROR - 2019-04-02 17:19:07 --> Severity: Warning --> file_get_contents(http://localhost/api/api/global/countries?token=123): failed to open stream: HTTP request failed! HTTP/1.0 500 Internal Server Error
+ E:\server\htdocs\bob\application\controllers\Home.php 14
+ERROR - 2019-04-02 17:19:07 --> Severity: Notice --> Trying to get property 'data' of non-object E:\server\htdocs\bob\application\controllers\Home.php 14
+ERROR - 2019-04-02 17:19:07 --> Severity: error --> Exception: Call to undefined method Home::getOTAdata() E:\server\htdocs\bob\application\controllers\Home.php 15
